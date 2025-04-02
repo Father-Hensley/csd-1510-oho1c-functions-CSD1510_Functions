3dfe58decbf0f6cce6ac0d6e83c3477f626b0bea
Father-Hensley <jhensley0818@gmail.com>
2025-04-02 16:46:51 -0400

    Update addnumbers.py

--- addnumbers.py
+++ addnumbers.py
@@ -1,2 +1,18 @@
 #This is a teacher led demo.
 #Watch this video or follow along in class.
+
+def AddTwoNumbers(number1, number2): #Function that returns the sum of two variables
+    result = number1 + number2
+    return result 
+
+def PrintHelloTen(): #Function looping 10 times
+    for _ in range(10):
+        print("Hello")
+x = 5 #Variables
+y = 10
+result = AddTwoNumbers(x,y) #Calling def function and keeping the result
+
+
+print(result) #Prints AddTwoNumbers function
+
+PrintHelloTen() #Calling def function to print hello 10 times
