82d71eb25e70a2c71507aea7e4b40a6ab98da92b
Father-Hensley <jhensley0818@gmail.com>
2025-04-07 10:02:28 -0400

    Update checkinput.py

--- checkinput.py
+++ checkinput.py
@@ -4,3 +4,14 @@
 #The function will continue to prompt the user to enter an integer until a proper integer is entered.
 #The most direct way of doing this would be using a try block, which has not been covered yet. You will need to research this.
 #Write supporting code to call the function, and then display the number that was entered.
+
+def get_int(pmessage, emessage): #This is the fuction
+    while True: #While loop focusing on true statements
+        try: #NEW-Allows the program to continue running with a known error
+            x = int(input(pmessage)) #Informs for an input. Set for an integer
+            return x #return value for x
+        except ValueError: #NEW-Creates an exception allowing the function to continue
+            print(emessage)
+
+integer = get_int("Enter a number: ", "Do you know what a number is?") #Like a print statement. Gives the prompt to the user
+print(f"{integer} is what you entered") 
